1e911bd13bb86170ca32006150f64c32e3e941e2
North_Samurai <59742048+omega2705@users.noreply.github.com>
2022-11-14 19:34:42 +0300

    Доделан немного гуи,связать все и доделать логику gui config v 0.8

--- test_ui.py
+++
@@ -1,8 +0,0 @@
-from PyQt5 import uic ,QtWidgets #выдает ошибку,но не трогать,все работает
-import sys
-
-app = QtWidgets.QApplication([])
-win = uic.loadUi("mydesign.ui")  # расположение вашего файла .ui
-
-win.show()
-sys.exit(app.exec())
